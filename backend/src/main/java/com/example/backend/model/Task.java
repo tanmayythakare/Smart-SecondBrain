a7232e6c5d1a2407410abd4cb049c0c1cab533b3
justTanmay <tanmayrthakare@gmail.com>
2026-01-25 13:21:32 +0530

    Week 1 : wrap-up with clean backend basline

---
+++ backend/src/main/java/com/example/backend/model/Task.java
@@ -0,0 +1,15 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+}
