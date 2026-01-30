90c1d6d30ada0457675c935909fd87f84a1cc356
justTanmay <tanmayrthakare@gmail.com>
2026-01-30 19:17:12 +0530

    Fixed the CORS

--- backend/src/main/java/com/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://thisissecondbrain.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
