ff216797e231515f44fc4ff8681c8dc26d48db10
tanmayythakare <tanmayrthakare@gmail.com>
2026-05-03 20:24:46 +0530

    chore: remove _trash folder from repository

--- _trash/backend/NoteAssistRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.dto.ai;
-
-import lombok.Data;
-
-@Data
-public class NoteAssistRequest {
-    private String content;
-    private String instruction; // e.g., "summarize", "polish", "expand"
-}
