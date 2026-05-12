7cf9d4f30a77d235bd5febf03a951a04618d14e6
tanmayythakare <tanmayrthakare@gmail.com>
2026-05-13 00:44:23 +0530

    fix: redeploy

--- backend/src/main/java/com/example/backend/config/JacksonConfig.java
+++ backend/src/main/java/com/example/backend/config/JacksonConfig.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
-
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         return mapper;
+
     }
 }
